666cf2fb32e977be44cd4dc4415e1553a1337573
Andreas Dann <adann@mail.uni-paderborn.de>
2022-09-01 11:05:05 +0200

    add comments to script

--- prepare-inputfile.sh
+++ prepare-inputfile.sh
@@ -6,9 +6,11 @@ curDir=${PWD}
 wget $ZENODOFILE -P /tmp/
 mytmpdir=$(mktemp -d 2>/dev/null || mktemp -d -t 'upcytmpdir')
 unzip /tmp/experimental-results_dataset.zip -d $mytmpdir
+# rename folder to match expected input
 mv $mytmpdir/results_update-steps $mytmpdir/projects
+# create file with expected name
 cd $mytmpdir && zip -r project_input_recommendation.zip ./projects
 mv -f project_input_recommendation.zip $curDir
+# clean up
 cd $curDir
 rm -rf $mytmpdir
-
