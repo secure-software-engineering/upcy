29563ad54a1f6d468ee372a74dbb2cdf5f85b7a4
Andreas Dann <adann@mail.uni-paderborn.de>
2025-06-03 23:56:18 +0200

    add goblin to dep

--- pipeline/src/main/java/tools/GoblinRun.java
+++ pipeline/src/main/java/tools/GoblinRun.java
@@ -1,5 +1,17 @@
 package tools;
 
-public class GoblinRun {
+import de.upb.upcy.pipeline.AbstractToolRun;
+import de.upb.upcy.update.build.NaiveUpdateStep;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.Map;
 
+public class GoblinRun extends AbstractToolRun {
+
+  @Override
+  public void runTool(Path projectDir, String projectName, Path csvFile, Path outputDir,
+      Map<String, List<NaiveUpdateStep>> naiveUpdatesStepsPerModule) {
+
+    // invoke goblin update
+  }
 }
