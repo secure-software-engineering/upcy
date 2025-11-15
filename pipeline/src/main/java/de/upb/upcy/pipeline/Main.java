651c9f9d3583c794bb9596ffb98d20fa46e81d48
Andreas Dann <adann@mail.uni-paderborn.de>
2025-11-15 16:51:50 +0100

    add argument to choose tool

--- pipeline/src/main/java/de/upb/upcy/pipeline/Main.java
+++ pipeline/src/main/java/de/upb/upcy/pipeline/Main.java
@@ -6,12 +6,30 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.core.LoggerContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import tools.CAROLPipeline;
 import tools.GoblinPipeline;
 import tools.MvnPipeline.MavenPipelineTool;
+import tools.PipelineTool;
 
 public class Main {
 
   private static Logger LOGGER = LoggerFactory.getLogger(Main.class);
+
+
+  public static PipelineTool chooseTool(String toolname) {
+    switch (toolname.toLowerCase()) {
+      case "maven":
+        return new MavenPipelineTool();
+      case "mvn":
+        return new MavenPipelineTool();
+      case "goblin":
+        return new GoblinPipeline();
+      case "carol":
+        return new CAROLPipeline();
+      default:
+        throw new IllegalArgumentException("Unknown toolname: " + toolname);
+    }
+  }
 
   public static void main(String[] args) throws IOException {
     System.out.println(org.slf4j.impl.StaticLoggerBinder.getSingleton().getLoggerFactoryClassStr());
@@ -21,18 +39,16 @@ public class Main {
         "Config file: " + ctx.getConfiguration().getConfigurationSource().getLocation());
     System.out.println("Root logger level: " + ctx.getConfiguration().getRootLogger().getLevel());
 
-    if (args.length < 2) {
+    if (args.length < 4) {
       LOGGER.error("No arguments given");
       return;
     }
     String rootDir = args[0];
     String workingDirectory = args[1];
-
-//    ToolPipeline pipelineTool =
-//        new ToolPipeline(Paths.get(rootDir), Paths.get(workingDirectory), new MavenPipelineTool());
+    String toolname = args[2];
 
     ToolPipeline pipelineTool =
-        new ToolPipeline(Paths.get(rootDir), Paths.get(workingDirectory), new GoblinPipeline());
+        new ToolPipeline(Paths.get(rootDir), Paths.get(workingDirectory), chooseTool(toolname));
 
     pipelineTool.execute();
   }
